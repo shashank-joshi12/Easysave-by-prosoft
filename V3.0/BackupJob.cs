176f85fe4cd003dda7cc307f4ed2e797c266b453
shashank-joshi12 <151512888+shashank-joshi12@users.noreply.github.com>
2023-12-21 22:52:05 +0100

    Add files via upload

---
+++ V3.0/BackupJob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easysave_v2._0.model
+{
+    public class BackupJob
+    {
+        
+        public string SourceDir { get; set; }
+        public string TargetDir { get; set; }
+        public string SaveName { get; set; }
+        public int Type { get; set; }
+        public bool LogType { get; set; }
+        
+
+        public BackupJob(string saveName, string sourceDir, string targetDir, int type, bool logType)
+        {
+            SaveName = saveName;
+            SourceDir = sourceDir;
+            TargetDir = targetDir;
+            Type = type;
+            LogType = logType;
+            
+        }
+    }
+}
---
+++ V3.0/JsonLoggerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easysave_v2._0.model
+
+{
+    class JsonLoggerData
+    {
+        
+        public string SourceDir { get; set; }
+        public string TargetDir { get; set; }
+        public string SaveName { get; set; }
+        public string BackupDate { get; set; }
+        public string ElapsedTime { get; set; }
+        public long TotalSize { get; set; }
+        public string CryptTime { get; set; }
+
+    }
+}
